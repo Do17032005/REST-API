9531f99fb71b759ba47843fe21308faeb8e2d5a0
Do17032005 <23010581@st.phenikaa-uni.edu.vn>
2026-06-30 21:36:29 +0700

    Initial commit

---
+++ session1/bai2/demo/src/main/java/com/example/demo/model/entity/nh.java
@@ -0,0 +1,14 @@
+package com.example.demo.model.entity;
+import lombok.*;
+import lombok.Setter;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class nh {
+    private int id;
+    private String name;
+    private String address;
+    private String contact;
+    private String status;
+}
